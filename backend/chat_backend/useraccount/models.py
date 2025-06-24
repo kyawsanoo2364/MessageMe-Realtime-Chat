1482d086d03b1303ddee77d349c95a6f35f17bf1
Kyaw San Oo <skyaw6736@gmail.com>
2025-06-24 10:02:32 +0630

    update dockerfile

--- backend/chat_backend/useraccount/models.py
+++ backend/chat_backend/useraccount/models.py
@@ -45,7 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
-    last_login = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(auto_now=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
